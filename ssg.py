e9313a088360aabbde7f3024f1c2f246d585c046
Whitney.Beehler <Z000897@m6dy9k7wmt.target.com>
2022-09-06 15:53:14 -0500

    Module 2

--- ssg.py
+++ ssg.py
@@ -1,9 +1,10 @@
+import ssg.parsers
 import typer
 from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
     Site(**config).build()
 
 
