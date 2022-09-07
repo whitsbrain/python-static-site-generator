9673556bd90e0338f1afbadde4e62a38ad0026ce
Whitney.Beehler <Z000897@m6dy9k7wmt.target.com>
2022-09-07 09:34:35 -0500

    Module 4 2nd push

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -44,7 +44,7 @@ class MarkdownParser(Parser):
         html = markdown(content.body)
         self.write(path, dest, html)
         sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML.  Metadata: {}\n".format(path.name, content)
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
 
@@ -56,5 +56,5 @@ class ReStructuredTextParser(Parser):
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML.  Metadata: {}\n".format(path.name, content)
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
